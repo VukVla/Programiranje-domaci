c55ee39a95fca33b7e6ed1e4458aa2317325c767
VukVla <72102722+VukVla@users.noreply.github.com>
2020-10-15 20:13:09 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,17 +4,26 @@
 
 int main()
 {
-    char string[100];
-    FILE* PokNaDat = fopen("tekstualni_dokument.txt","w+");
-    printf("unesite ime, prezime i broj u dnevniku:");
-    char string1[100], string2[100];
-    int broj;
-    gets(string1);
-    gets(string2);
-    scanf("%d", &broj);
-    fputs(string1, PokNaDat);
-    fputs(string2, PokNaDat);
-    fprintf(PokNaDat, "%d", broj);
-    fclose(PokNaDat);
+    char a;
+    int brojac=0, i;
+    printf("unesite karakter za promenu: ");
+    scanf("%c", &a);
+    FILE* pPRIMER = fopen("PRIMER.txt","r");
+    int n = strlen(pPRIMER);
+    char znak;
+    for(i=1; i>0; )
+    {
+        znak = fgetc(pPRIMER);
+        if(a==znak)
+        {
+            brojac++;
+        }
+        if(znak == EOF)
+        {
+            break;
+        }
+    }
+    printf("Znak %c se pojavljuje %d put/a", a, brojac);
+    fclose("primer.txt");
     return 0;
 }
